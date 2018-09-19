5fc9976d1ae90aff70aeea5dbe96d3c256d925ec
Brien Dieterle <briend@gmail.com>
2018-09-18 21:55:43 -0700

    Revert "spectral and subtractive settings added to brush mode"
    This reverts commit ff3705242459cb4a14f7b5f206687c01823e260a.

--- lib/tiledsurface.hpp
+++ lib/tiledsurface.hpp
@@ -72,17 +72,12 @@ public:
                  float lock_alpha = 0.0,
                  float colorize = 0.0,
                  float posterize = 0.0,
-                 float posterize_num = 0.0,
-                 float subtractive = 0.0,
-                 float spectral = 0.0,
-                 float multiply = 0.0,
-                 float gamma = 1.0
+                 float posterize_num = 0.0
                  ) {
 
     return mypaint_surface_draw_dab((MyPaintSurface *)c_surface, x, y, radius, color_r, color_g, color_b,
                              opaque, hardness, color_a, aspect_ratio, angle,
-                             lock_alpha, colorize, posterize, posterize_num,
-                             subtractive, spectral, multiply, gamma);
+                             lock_alpha, colorize, posterize, posterize_num);
   }
 
   std::vector<double> get_color (double x, double y, double radius) {
